fadc7e5a9df8f3be5048a9bc42078a30f3018ea8
shibiyas <shibiyas@gmail.com>
2023-12-29 12:25:56 +0530

    xxx

--- src/component/Home/navbar/Navbar.jsx
+++ src/component/Home/navbar/Navbar.jsx
@@ -17,6 +17,16 @@ const Navbar = () => {
     useEffect(()=>{
       getName();
     },[])
+
+    const Logout = () => {
+      const isConfirmed = window.confirm("Are you sure you want to logout?");
+      if (isConfirmed) {
+        localStorage.clear();
+        naviagate("/")
+  
+      }
+    };
+
   return (
     <div>
       <div className="nav-all">
@@ -26,11 +36,7 @@ const Navbar = () => {
           <div className="col-lg-8 left-nav">
             <h3>CHANEL</h3>
             <div className="icons">
-           <Link className='icon' to='/addcustomer'>
-           <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z"/>
-</svg>
-           </Link>
+           
 
            <Link className='icon'> <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
@@ -41,7 +47,23 @@ const Navbar = () => {
 </svg></Link>
            <Link className='icon'> <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
   <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
-</svg> </Link>         
+</svg> </Link>  
+
+{msg ?(
+  <>
+   <Link className='signout' onClick={ Logout}><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+  <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+</svg></Link>    <i class="fa fa-user" aria-hidden="true"></i>
+<span id='msg'>{msg}</span>   
+  </>
+):(
+  <Link className='icon' to='/addcustomer'>
+           <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z"/>
+</svg>
+           </Link>
+)}
 
 {/* <div className="msg">
     hello{msg}
@@ -51,16 +73,13 @@ const Navbar = () => {
           </div>
         </div>
         <div className="lower">
-          <a href="">PRODUCT</a>
-          <a href="">CATEGORY</a>
-          <a href="">ABOUT</a>          
-          <a href="">CONTACT</a>          
+         <Link className='nav-btn'><a href="">PRODUCT</a></Link>
+         <Link className='nav-btn'> <a href="">CATEGORY</a></Link>
+          <Link className='nav-btn'><a href="">ABOUT</a>  </Link>        
+          <Link className='nav-btn'><a href="">CONTACT</a></Link>          
           
         </div>
-        <div className="msg">
-        <i class="fa fa-user" aria-hidden="true"></i>
-<span>{msg}</span>
-</div>
+      
 
           
 
