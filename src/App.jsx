8de3cc5a0742c199d6c2ff4964e9d39d75cd2e00
shibiyas <shibiyas@gmail.com>
2023-12-29 18:47:33 +0530

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import Adminlogin from './component/Admin/Adminlogin/Adminlogin'
 import Adminhome from './component/Admin/Adminhome/Adminhome'
 import Forgotpassword from './component/Admin/Forgot password/Forgotpassword'
 import Addcategory from './component/Admin/Adminhome/Addcategory/Addcategory'
-import Addproduct from './component/Admin/Adminhome/Addproduct/Addproduct'
+import Addproduct from './component/Admin/Adminhome/product/Addproduct/Addproduct'
 import Editcategory from './component/Admin/Adminhome/editcategory/Editcategory'
 import Home from './component/Home/Home'
 import Categoryproduct from './component/category-vise/Categoryproduct'
 import Addcustomer from './component/User/Customerreg/Addcustomer'
 import Logincustomer from './component/User/Logincust/Logincustomer'
+import Productdetails from './component/Admin/Adminhome/product/productdetails/Productdetails'
 
 function App() {
 
@@ -30,6 +31,8 @@ function App() {
             <Route path='/categoryproduct/:category' Component={Categoryproduct}/>
             <Route path='/addcustomer' Component={Addcustomer}/>
             <Route path='/logincustomer' Component={Logincustomer}/>
+            <Route path='/productdetails' Component={Productdetails}/>
+
           </Routes>
       </BrowserRouter>
     </>
